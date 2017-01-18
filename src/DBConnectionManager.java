318eb736e235833b0920bddb1fc0468a3b0b7143
DarreN <DarreN>
2017-01-18 13:46:22 +0800

    Database Connectiob by DarreN 1/18/17

--- src/DBConnectionManager.java
+++ src/DBConnectionManager.java
@@ -9,23 +9,39 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-public class DBConnectionManager {
-	private static String url = "jdbc:mysql://localhost:3306/projectnoove";    
+public class DBConnectionManager {   
     private static String driverName = "com.mysql.jdbc.Driver";   
-    private static String username = "root";   
-    private static String password = "";
     private static Connection connection;
     private static String urlstring;
-    private static Statement stmt;
-    private Context envContext = null;
+    private Statement stmt = null;
 
-    public DBConnectionManager() throws ClassNotFoundException, SQLException{
-		Class.forName(driverName);
-		this.connection = DriverManager.getConnection(url, username, password);
-	}
-	
-	public Connection getConnection(){
-		return this.connection;
-	}
-
+    public DBConnectionManager(String url, String username, String password) throws ClassNotFoundException, SQLException{
+            Class.forName(driverName);
+            this.connection = DriverManager.getConnection(url, username, password);
+    }
+    
+    public ResultSet query(String query) { 
+        ResultSet rs = null;
+        try {
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        return rs;
+    }
+    
+    public Boolean execute(String query) {
+       Boolean result;
+       try{
+            stmt = connection.createStatement();
+            result = stmt.execute(query);
+       }catch(SQLException e) {
+           result = false;
+           e.printStackTrace();
+       }
+       
+       return result;
+    }
 }
